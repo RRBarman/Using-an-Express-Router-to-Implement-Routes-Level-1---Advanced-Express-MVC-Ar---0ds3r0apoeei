c34cee0714134df682237b88e0ab46d32498fc78
RAUNAK ROY BARMAN <48653401+RRBarman@users.noreply.github.com>
2023-04-19 18:12:17 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+/*const fs = require('fs');
 const express = require('express');
 const app = express();
 //Aim: With the help of router, get all the product with router.GET request and create a product with router.POST request
@@ -32,5 +32,68 @@ router.post('/api/v1/product', (req, res) => {
 
 //Registering our Router
 //Write here to register router
+
+module.exports = app;
+*/
+
+const express = require('express');
+const fs = require('fs');
+const router = express.Router();
+
+const products = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/product.json`));
+
+router.get('/api/v1/product', (req, res) => {
+  try {
+    res.status(200).json({
+      status: 'success',
+      results: products.length,
+      data: {
+        product: products,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'error',
+      message: error.message,
+    });
+  }
+});
+
+router.post('/api/v1/product', (req, res) => {
+  try {
+    const { title, price } = req.body;
+    if (!title || !price) {
+      throw new Error('Title and price are required');
+    }
+    const newProduct = {
+      id: products.length + 1,
+      title,
+      price,
+    };
+    products.push(newProduct);
+    fs.writeFile(
+      `${__dirname}/../dev-data/product.json`,
+      JSON.stringify(products),
+      (err) => {
+        if (err) throw new Error('Error creating product');
+        res.status(201).json({
+          status: 'success',
+          data: {
+            product: newProduct,
+          },
+        });
+      }
+    );
+  } catch (error) {
+    res.status(404).json({
+      status: 'error',
+      message: error.message,
+    });
+  }
+});
+
+const app = express();
+app.use(express.json());
+app.use(router);
 
 module.exports = app;
